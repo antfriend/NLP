b6cd7e057fba385504f7800a314471b8930025b6
Dan Ray <antfriend@gmail.com>
2012-04-12 09:34:30 -0600

    add this

---
+++ loadcontent.py
@@ -0,0 +1,35 @@
+#***************************************************************
+#*                                                             *
+#*                  load a folder of content                   *
+#*                                                             *
+#***************************************************************
+
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+print "*****************************************"
+print "********** Content Loader *************"
+print "*****************************************"
+print "usage..."
+print "import nltk"
+print "md = loadcontent.Get_text()"
+print "  ~ or ~ "
+print "cats = loadcontent.Get_text('kb_xml/hwxml/categories')"
+print " "
+print " "
+
+def Get_text(corpus_root = '/release/'):    
+    wordlists = PlaintextCorpusReader(corpus_root, '.*')
+    print "corpus read from " + corpus_root + " ..."
+    raw = wordlists.raw()
+    print "corpus rawed ..."
+    tokens = nltk.word_tokenize(raw)
+    print "corpus tokenized ..."
+    text = nltk.Text(tokens)
+    print "corpus textified ..."        
+    simple_md = [word.lower() for word in text if word.isalpha()]
+    print "corpus lowercased and alphafied ..."
+    simple_md = [word for word in simple_md if word != 'source']
+    print "keyword *source* removed ..."
+    print "DONE!"
+    return simple_md
+
