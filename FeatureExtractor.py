43d14f2f2de9520821a0b206bf92faaac1b41d10
Dan Ray <antfriend@gmail.com>
2012-04-12 16:32:04 -0600

    mostly functional, classifying

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -6,10 +6,17 @@ import loadwords
 word_features = []
 
 #iterate a list of facet classes
-da_FacetValues = os.listdir('FacetValues')
+#da_FacetValues = os.listdir('FacetValues')
+da_Facet_Dir = 'lil_FacetValues/'
+#da_Facet_Dir = 'FacetValues/'
+da_FacetValues = os.listdir(da_Facet_Dir)
+
+print '*********************************'
+print '********* Collecting ***********'
+print '*********************************'
 
 for da_FacetValue in da_FacetValues:
-    f = open('FacetValues/'+da_FacetValue, 'r')
+    f = open(da_Facet_Dir+da_FacetValue, 'r')
     for hwid in f.read().split('\n'):
         print da_FacetValue.rstrip('.txt') + ':' + hwid
         #iterate hwids
@@ -29,6 +36,27 @@ def document_features(document):
         features['contains(%s)' % word] = (word in document_words)
     return features
 
+print '*********************************'
+print '********* Classifying ***********'
+print '*********************************'
 
+featuresets = []
+for da_FacetValue in da_FacetValues:
+    f = open(da_Facet_Dir + da_FacetValue, 'r')
+    for hwid in f.read().split('\n'):
+        print da_FacetValue.rstrip('.txt') + ':' + hwid
+        d = 'kb_xml' + hwid
+        c = da_FacetValue
+        featuresets = featuresets + [document_features(d), c] 
+    f.close()
+print '*********************************'
+print '********* Classified! ***********'
+print '*********************************'
 
+train_set, test_set = featuresets[5:], featuresets[:5]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+print '*** blab! ***'
+
+print nltk.classify.accuracy(classifier, test_set)
 
